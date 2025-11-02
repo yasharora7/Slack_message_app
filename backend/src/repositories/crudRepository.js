225f2fade48843290b73b3a0e86ddca9de8cc037
yasharora7 <y79arora@gmail.com>
2025-11-02 19:24:16 +0530

    git push origin main

---
+++ backend/src/repositories/crudRepository.js
@@ -0,0 +1,24 @@
+export default function crudRepository(model){
+    return {
+        create: async function (data) {
+            const newDoc= await model.create(data);
+            return newDoc
+        },
+        getAll: async function (){
+            const allDocs = await model.find();
+            return allDocs;
+        },
+        getById: async function () {
+            const doc = await model.findbyId(id);
+            return doc;
+        },
+        delete: async function (id){
+            const response = await model.findByIdAndDelete(id);
+            return response;
+        },
+        update: async function (id, data){
+            const updateDoc = await model.findByIdAndUpdate(id,data,{new: true});
+            return updateDoc;
+        }
+    }
+}
---
+++ backend/src/repositories/userRepository.js
@@ -0,0 +1,33 @@
+import User from '../schema/user.js';
+import crudRepository from './crudRepository.js';
+
+const userRepository={
+    ...crudRepository(User),
+
+    getByEmail: async function (email){
+        const user = await User.findOne({email});
+        return user;
+    },
+    
+    getByUsername: async function (username){
+        const user= await User.findone({username});
+        return user;
+    }
+};
+
+export default userRepository;
+
+// export const getUserByEmail=(email)=>{};
+
+// export const getUserByName=(name)=>{};
+
+// export const createUser=(user)=>{};
+
+// export const getUser=()=>{};
+
+// export const getUserById=(id)=>{};
+
+// export const deleteUser=(id)=>{};
+
+// export const updateUser=(id,user)=>{};
+
